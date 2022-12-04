507f5afd614d41fe56b928b3285679fcbfaf37fa
Arinaho Muleba <arinaomuleluz@gmail.com>
2022-12-04 10:03:25 +0200

    lols go

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+let meme = ["meme 1", "meme 2", "meme 3"]
+
+function NavBar() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <nav className='navBar'>
+      <img src='https://www.designevo.com/res/temsplates/thumb_small/meme-sunglasses-laugh.png' className='nav--img' alt='oops'></img>
+      <h1 className='nav--title'>Meme Generator</h1>
+    </nav>);
+}
+
+function Body() {
+  return (
+    <div className='body'>
+      <img src={memeImage} className='body--img' alt='oops'></img>
+    </div>
+  )
+}
+
+
+function NewMemeButton() {
+  return (
+    <div className='newMemeButton'>
+      <button className='newMemeButton--button'>Generate New Meme</button>
+    </div>
+  )
+}
+
+function Footer() {
+  return (
+    <div className='footer'>
+      <p className='footer--text'>Made with ❤️ by
+        <a href='https://www.instagram.com/am_stylesrsa/' className='footer--link'> Arinaho </a>
+
+      </p>
     </div>
   );
 }
+
+
+
+function App() {
+  return (
+
+    <div className="App">
+      <NavBar />
+      <Body />
+      <NewMemeButton />
+      <Footer />
+
+    </div>
+  );
+}
+
+const memeImage = "https://i.imgflip.com/1bij.jpg";
 
 export default App;
